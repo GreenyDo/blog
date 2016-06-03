c434dba4b398975e934e108ade052c18c441cdc3
GreenyDo <GreenyDo>
2016-06-03 18:30:15 +0800

    2016/06/03 18:30

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
-	var $ring=$("#ring")
-	$("#githubicon").mouseover(function(){
-		$ring.stop().animate({'display':'show'},500);
+	// github操作
+	var $ring=$('#ring');
+	var $github=$('#github');
+	$github.mouseover(function(){
+		$ring.stop().animate({'border-color':'#070'},400);
 	}).mouseout(function(){
-		$ring.stop().animate({'display':'none'},500);
+		$ring.stop().animate({'border-color':'#888'},200);
+	});
+	$github.click(function(){
+		window.open('https://github.com/greenydo');
+	});
+	// 邮件操作
+	var $email=$('#email');
+	var $qemail=$('#myemail p');
+	$email.mouseover(function(){
+		$(this).addClass('myemaila');
+		$qemail.show();
+	}).mouseout(function(){
+		$(this).removeClass('myemaila');
 	});
 });
