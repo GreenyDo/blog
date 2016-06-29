6fd628e5032696b10bc9abd822ed80d306f72c9e
GreenyDo <GreenyDo>
2016-06-29 17:29:10 +0800

    icon  and some article

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -2,7 +2,8 @@ $(function(){
 	$articleTag=$("#accordion .submenu li");
 	$articleTag.click(function(){
 		var id=this.id;
-		var idUrl="tpls/"+id+".html";
+		var item = this.className;
+		var idUrl="tpls/"+item+"/"+id+".html";
 		$("article").load(idUrl);
 	});
 });
